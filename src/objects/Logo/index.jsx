5f4eeb08a9e5d106d6751436364c6d9d60994ba4
dgmaikon <dgmaikon@hotmail.com>
2023-03-22 06:15:58 -0300

    Criou o Logo e adicionamos no App.js

---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import dgLogo from "../../img/logo.png";
+
+const Logo = () => {
+  return (
+    <>
+      <img src={dgLogo} alt="Logo DG" />
+    </>
+  );
+};
+
+export default Logo;
