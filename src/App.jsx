5573f4e04e7608fa0de1ae95f881a4dc47e86051
dgmaikon <dgmaikon@hotmail.com>
2023-03-28 07:59:41 -0300

    Criamos o component Hastag e ja definimos o layout basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import React from "react";
-import Card from "./objects/Card";
-import Player from "./objects/Player";
 import Header from "./components/Header";
+import Hastag from "./components/Hastag";
 
 function App() {
   return (
     <>
       <Header />
-      <Card>
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="x" />
-
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="o" />
-      </Card>
+      <Hastag />
     </>
   );
 }
---
+++ src/components/Hastag/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./styles.css";
+
+import Card from "../../objects/Card";
+import Player from "../../objects/Player";
+
+const Hastag = () => {
+  return (
+    <Card>
+      <ul className="hastag">
+        <li className="item">
+          <Player player="x" />
+        </li>
+        <li className="item">
+          <Player player="o" />
+        </li>
+        <li className="item">
+          <Player player="x" />
+        </li>
+
+        <li className="item">
+          <Player player="o" />
+        </li>
+        <li className="item">
+          <Player player="x" />
+        </li>
+        <li className="item">
+          <Player player="x" />
+        </li>
+
+        <li className="item">
+          <Player player="x" />
+        </li>
+        <li className="item">
+          <Player player="o" />
+        </li>
+        <li className="item">
+          <Player player="o" />
+        </li>
+      </ul>
+    </Card>
+  );
+};
+
+export default Hastag;
