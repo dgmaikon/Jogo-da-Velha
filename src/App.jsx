c5fd1978a65b17706b9c743a287fb103f2dde4ea
dgmaikon <dgmaikon@hotmail.com>
2023-03-29 06:04:59 -0300

    Criou o object input

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import "./app.css";
 import Header from "./components/Header";
 import Hastag from "./components/Hastag";
 import Label from "./objects/Label";
+import Input from "./objects/Input";
 
 const App = () => (
   <main className="app">
     <Header />
     <Hastag />
+    <Input value="show" />
     <Label content="Mostrar Eventos" />
   </main>
 );
---
+++ src/objects/Input/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Input = () => <input type="checkbox" value="show" />;
+
+export default Input;
