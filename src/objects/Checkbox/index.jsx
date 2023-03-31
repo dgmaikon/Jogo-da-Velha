10c146bf34459699c711bdb33d3cfdf3458e53b7
dgmaikon <dgmaikon@hotmail.com>
2023-03-31 08:46:59 -0300

    Criar o checkbox com o before

--- src/objects/Checkbox/index.jsx
+++ src/objects/Checkbox/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Input from "../Input";
 import Label from "../Label";
+import "./styles.css";
 
 const Checkbox = ({ id = "", value = "", content = "" }) => (
   <>
-    <Input id={id} value={value} type="checkbox" content={content} />
+    <Input
+      className="input"
+      id={id}
+      value={value}
+      type="checkbox"
+      content={content}
+    />
     <Label htmlFor={id} content={content} />
   </>
 );
